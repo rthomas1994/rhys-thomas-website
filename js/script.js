e93ac50a8e20c3fc8a97d19b4f18d81b47f734c5
Rhys Thomas <rhysthomas1994@gmail.com>
2023-02-22 21:08:23 -0500

    New website design initial code push

--- js/script.js
+++ js/script.js
@@ -1,90 +1,270 @@
-var scrollOffset = 50;
+// var scrollOffset = 50;
 
 $(document).ready(function() {
 
-    if ($(window).width() < 700) {
-        scrollOffset = 120;
-    }
+	var currentYear = new Date().getFullYear();
 
-    // Check on page load
-    checkThemeSwitch();
+	// Set copyright year
+	$("#copyright-year").html(currentYear);
 
-    $("#theme_switch").change(function() {
-        checkThemeSwitch();
-    });
+	// if ($(window).width() < 700) {
+	// 	scrollOffset = 120;
+	// }
 
-    $('[id^="nav_"]').click(function() {
+	// Light/dark theme change
+	const lightButtons = $("#light_button, #light_button_mobile");
+	const darkButtons = $("#dark_button, #dark_button_mobile");
 
-        var clickedSection = $(this).attr("id").replace("nav_", "");
+	darkButtons.click(function() {
+		$("body").removeClass("light").addClass("dark");
+		darkButtons.toggleClass("display-none");
+		lightButtons.toggleClass("display-none");
+	});
 
-        // Scroll to clicked section
-        $("html, body").animate({
-            scrollTop: $("#" + clickedSection).offset().top - scrollOffset
-        }, 400)
+	lightButtons.click(function() {
+		$("body").removeClass("dark").addClass("light");
+		lightButtons.toggleClass("display-none");
+		darkButtons.toggleClass("display-none");
+	});
 
-        // Remove active class from all
-        $(".navItem").removeClass("navItem--active");
+	// Show & hide mobile menu
+	$(".menu-button--open").click(function() {
+		$(".mobile-nav").addClass("mobile-nav--visible");
+	});
 
-        // Add active class to clicked item
-        $(this).addClass("navItem--active");
+	// Close mobile menu on click of x button
+	$(".menu-button--close, .mobile-nav__links .nav-link > a").click(function() {
+		$(".mobile-nav").removeClass("mobile-nav--visible");
+	});
 
-    });
+	// Scroll nav shadows
+	// $(window).scroll(function() {     
+  //   var scroll = $(window).scrollTop();
+  //   if (scroll > 0) {
+  //       $("nav").addClass("shadow");
+  //   }
+  //   else {
+  //       $("nav").removeClass("shadow");
+  //   }
+	// });
 
-    // Add active class for workItem on hover
-    $(".workItem").hover(function() {
-        $(this).closest(".workItem").toggleClass("workItem--active");
-    });
+	// $(window).scroll(function() {
+  //   clearTimeout($.data(this, 'scrollTimer'));
+  //   $.data(this, 'scrollTimer', setTimeout(function() {
+  //       $("nav").removeClass("shadow");
+  //   }, 250));
+	// });
 
-    // Show relevant work item when using menu
-    $("[id^='menu_']").click(function() {
+	// Skill item data
+	const skillData = [
+		{
+			skill: 'Figma',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/figma-logo.png',
+			alt: 'Figma icon',
+			level: '5+ years experience',
+			type: 'design'
+		},
+		{
+			skill: 'Sketch',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/sketch-logo.png',
+			alt: 'Sketch app icon',
+			level: '2-3 years experience',
+			type: 'design'
+		},
+		{
+			skill: 'Adobe CC',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/adobe-cc-logo.png',
+			alt: 'Adobe CC icon',
+			level: '2-3 years experience',
+			type: 'design'
+		},
+		{
+			skill: 'Web Accessibility',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/accessibility-logo.png',
+			alt: 'Web accessibility icon',
+			level: '2-3 years experience',
+			type: 'design'
+		},
+		{
+			skill: 'InVision',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/invision-logo.png',
+			alt: 'InVision icon',
+			level: '1-2 years experience',
+			type: 'design'
+		},
+		{
+			skill: 'HTML5',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/html-logo.png',
+			alt: 'CSS3 icon',
+			level: '5+ years experience',
+			type: 'development'
+		},
+		{
+			skill: 'CSS3',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/css-logo.png',
+			alt: 'CSS3 icon',
+			level: '5+ years experience',
+			type: 'development'
+		},
+		{
+			skill: 'SASS/SCSS',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/sass-logo.png',
+			alt: 'SASS icon',
+			level: '5+ years experience',
+			type: 'development'
+		},
+		{
+			skill: 'JavaScript',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/javascript-logo.png',
+			alt: 'JavaScript icon',
+			level: '3-4 years experience',
+			type: 'development'
+		},
+		{
+			skill: 'jQuery',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/jquery-logo.png',
+			alt: 'jQuery icon',
+			level: '3-4 years experience',
+			type: 'development'
+		},
+		{
+			skill: 'GitHub',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/github-logo.png',
+			alt: 'GitHub icon',
+			level: '2-3 years experience',
+			type: 'development'
+		},
+		{
+			skill: 'Bootstrap',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/bootstrap-logo.png',
+			alt: 'Bootstrap icon',
+			level: '2-3 years experience',
+			type: 'development'
+		},
+		{
+			skill: 'ReactJS',
+			icon: '/Users/Rhys/Pictures/rhys-website-media/react-logo.png',
+			alt: 'ReactJS icon',
+			level: '0-1 years experience',
+			type: 'development'
+		}
+	];
 
-        // Get number of clicked menu item
-        var menuNumber = $(this).attr("id").replace("menu_", "");
+	var skillsContainer = $("#skills_container");
 
-        // Hide all workItems
-        $(".workItem").fadeOut(400);
-        // $(".workItem").addClass("displayNone");
+	// Dynamically build skill items
+	$(skillData).each(function(k, v) {
+		var element = `<div class="skill" data-skill-type="${v.type}">
+											<div class="skill__icon">
+												<img src="${v.icon}" class="" alt="${v.alt}" />
+											</div>
+											<div class="skill__info">
+												<p>${v.skill}</p>
+												<span>${v.level}</span>
+											</div>\
+										</div>`;
+		skillsContainer.append(element);
+	});
 
-        setTimeout(function() {
-            // Show relevant workItem
-            // $("#workItem_" + menuNumber).removeClass("displayNone");
-            $("#workItem_" + menuNumber).fadeIn(400);
-        }, 400);
+	// When skill filter changes
+	$("input[name='skillFilter']").change(function() {
+		var checkedValue = $("input[name='skillFilter']:checked").val();
+		// Remove active styling from all filter buttons
+		$(".filter-item").removeClass("filter-item--active");
+		// Add active styling to relevant filter button
+		$(".filter-item[data-filter-value='"+checkedValue+"']").addClass("filter-item--active");
+		// Hide all skill items
+		$(".skill").hide();
+		if (checkedValue === "all") {
+			$(".skill").fadeIn(500);
+		} else {
+			$(".skill[data-skill-type='"+checkedValue+"']").fadeIn(500);
+		}
+	});
 
-        // Remove all active menu classes
-        $(".menuItem").removeClass("menuItem--active")
+	var uiTag = `
+		<div class="work-tag work-tag--design">
+			<i class="bi bi-brush"></i>
+			<span>UI/UX Design</span>
+		</div>
+	`;
 
-        // Add active class to clicked item
-        $(this).addClass("menuItem--active");
+	var figmaTag = `
+		<div class="work-tag work-tag--design">
+			<i class="fab fa-figma"></i>
+			<span>Figma</span>
+		</div>
+	`;
 
-    });
+	var htmlTag = `
+		<div class="work-tag work-tag--dev">
+			<i class="fab fa-html5"></i>
+			<span>HTML</span>
+		</div>
+	`;
 
-    // Hover state on image for work section
-    // $(".workItem_hoverBox").hover(function() {
-    //     $(this).closest(".workItem").toggleClass("workItem--active");
-    // });
+	var cssTag = `
+		<div class="work-tag work-tag--dev">
+			<i class="fab fa-css3"></i>
+			<span>CSS3</span>
+		</div>
+	`;
 
+	var jsTag = `
+		<div class="work-tag work-tag--dev">
+			<i class="bi bi-code-slash"></i>
+			<span>JavaScript</span>
+		</div>
+	`;
 
+	var desktopTag = `
+		<div class="work-tag work-tag--design">
+			<i class="bi bi-display"></i>
+			<span>Desktop Design</span>
+		</div>
+	`;
 
+	var mobileTag = `
+		<div class="work-tag work-tag--design">
+			<i class="bi bi-phone"></i>
+			<span>Mobile Design</span>
+		</div>
+	`;
 
-    // On scroll remove active class from nav items
-    // $(window).scroll(function() {
+	var responsiveTag = `
+		<div class="work-tag work-tag--design">
+			<i class="bi bi-phone"></i>
+			<span>Responsive Design</span>
+		</div>
+	`;
 
-    //     // Remove active class from all
-    //     $(".navItem").removeClass("navItem--active");
+	var nocodeTag = `
+		<div class="work-tag work-tag--dev">
+			<i class="bi bi-hand-index"></i>
+			<span>No-Code Platform</span>
+		</div>
+	`;
 
-    // });
+	const tagList = [
+		{
+			uidesign: uiTag,
+			figma: figmaTag,
+			html: htmlTag,
+			css: cssTag,
+			javascript: jsTag,
+			desktop: desktopTag,
+			mobile: mobileTag,
+			responsive: responsiveTag,
+			nocode: nocodeTag
+		}
+	];
+
+	// Add all tags
+	$(".work-tag-outer").each(function() {
+		var currentTagOuter = $(this);
+		var tagName = String(currentTagOuter.data("tag-name"));
+		// Find the relevant item in the tagList array and append it
+		currentTagOuter.append(tagList[0][tagName]);
+	});
 
 });
-
-function checkThemeSwitch() {
-
-    if ($("#theme_switch").is(':checked')) {
-        $(".toggleSwitch_slider").addClass("toggleSwitch_slider--checked");
-        $("body").removeClass("lightMode");
-    } else {
-        $(".toggleSwitch_slider").removeClass("toggleSwitch_slider--checked");
-        $("body").addClass("lightMode");
-    }
-
-}
