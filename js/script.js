abc9a996db8f14970dc688d160243de719cb4374
Rhys Thomas <rhysthomas1994@gmail.com>
2025-02-22 23:01:04 -0500

    Launching new website design and updates

--- js/script.js
+++ js/script.js
@@ -22,11 +22,6 @@ $(document).ready(function() {
 	$("#copyright-year").html(currentYear);
 
 
-
-	// if ($(window).width() < 700) {
-	// 	scrollOffset = 120;
-	// }
-
 	// Light/dark theme change
 	const lightButtons = $("#light_button, #light_button_mobile");
 	const darkButtons = $("#dark_button, #dark_button_mobile");
@@ -53,244 +48,34 @@ $(document).ready(function() {
 		$(".mobile-nav").removeClass("mobile-nav--visible");
 	});
 
-	// Scroll nav shadows
-	// $(window).scroll(function() {     
-  //   var scroll = $(window).scrollTop();
-  //   if (scroll > 0) {
-  //       $("nav").addClass("shadow");
-  //   }
-  //   else {
-  //       $("nav").removeClass("shadow");
-  //   }
-	// });
+	// Shrink nav on scroll
 
-	// $(window).scroll(function() {
-  //   clearTimeout($.data(this, 'scrollTimer'));
-  //   $.data(this, 'scrollTimer', setTimeout(function() {
-  //       $("nav").removeClass("shadow");
-  //   }, 250));
-	// });
+	// Check scroll position on page load
+    if ($(window).scrollTop() > 80) {
+        $("nav.desktop-nav").addClass("desktop-nav--min");
+    }
 
-	// Skill item data
-	const skillData = [
-		{
-			skill: 'Figma',
-			icon: 'media/figma-logo.png',
-			alt: 'Figma icon',
-			level: '7+ years experience',
-			type: 'design'
-		},
-		{
-			skill: 'Sketch',
-			icon: 'media/sketch-logo.png',
-			alt: 'Sketch app icon',
-			level: '2-3 years experience',
-			type: 'design'
-		},
-		{
-			skill: 'Web Accessibility',
-			icon: 'media/accessibility-logo.png',
-			alt: 'Web accessibility icon',
-			level: '5+ years experience',
-			type: 'design',
-			invert: 'invert'
-		},
-		{
-			skill: 'Adobe CC',
-			icon: 'media/adobe-cc-logo.png',
-			alt: 'Adobe CC icon',
-			level: '2-3 years experience',
-			type: 'design'
-		},
-		{
-			skill: 'InVision',
-			icon: 'media/invision-logo.png',
-			alt: 'InVision icon',
-			level: '1-2 years experience',
-			type: 'design'
-		},
-		{
-			skill: 'HTML5',
-			icon: 'media/html-logo.png',
-			alt: 'CSS3 icon',
-			level: '5+ years experience',
-			type: 'development'
-		},
-		{
-			skill: 'CSS3',
-			icon: 'media/css-logo.png',
-			alt: 'CSS3 icon',
-			level: '5+ years experience',
-			type: 'development'
-		},
-		{
-			skill: 'SASS/SCSS',
-			icon: 'media/sass-logo.png',
-			alt: 'SASS icon',
-			level: '5+ years experience',
-			type: 'development'
-		},
-		{
-			skill: 'JavaScript',
-			icon: 'media/javascript-logo.png',
-			alt: 'JavaScript icon',
-			level: '3-4 years experience',
-			type: 'development'
-		},
-		{
-			skill: 'jQuery',
-			icon: 'media/jquery-logo.png',
-			alt: 'jQuery icon',
-			level: '3-4 years experience',
-			type: 'development'
-		},
-		{
-			skill: 'GitHub',
-			icon: 'media/github-logo.png',
-			alt: 'GitHub icon',
-			level: '5+ years experience',
-			type: 'development',
-			invert: 'invert'
-		},
-		{
-			skill: 'Bootstrap',
-			icon: 'media/bootstrap-logo.png',
-			alt: 'Bootstrap icon',
-			level: '2-3 years experience',
-			type: 'development'
-		},
-		{
-			skill: 'ReactJS',
-			icon: 'media/react-logo.png',
-			alt: 'ReactJS icon',
-			level: '0-1 years experience',
-			type: 'development'
-		}
-	];
+    // Check scroll position on scroll event
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 80) {
+            $("nav.desktop-nav").addClass("desktop-nav--min");
+        } else {
+            $("nav.desktop-nav").removeClass("desktop-nav--min");
+        }
+    });
 
-	var skillsContainer = $("#skills_container");
+	// Set no. of years experience
+	let yearsExperience = 7;
+	let lastUpdate = Date.now();
 
-	// Dynamically build skill items
-	$(skillData).each(function(k, v) {
-		var element = `<div class="skill" data-skill-type="${v.type}">
-											<div class="skill__icon">
-												<img src="${v.icon}" class="${v.invert}" alt="${v.alt}" />
-											</div>
-											<div class="skill__info">
-												<p>${v.skill}</p>
-												<span>${v.level}</span>
-											</div>\
-										</div>`;
-		skillsContainer.append(element);
-	});
+	setInterval(function() {
+	    if (Date.now() - lastUpdate >= 365 * 24 * 60 * 60 * 1000) { // 1 year in milliseconds
+	      yearsExperience++;
+	      lastUpdate = Date.now();
+	    }
+	}, 1000 * 60 * 60 * 24); // Checks once a day
 
-	// When skill filter changes
-	$("input[name='skillFilter']").change(function() {
-		var checkedValue = $("input[name='skillFilter']:checked").val();
-		// Remove active styling from all filter buttons
-		$(".filter-item").removeClass("filter-item--active");
-		// Add active styling to relevant filter button
-		$(".filter-item[data-filter-value='"+checkedValue+"']").addClass("filter-item--active");
-		// Hide all skill items
-		$(".skill").hide();
-		if (checkedValue === "all") {
-			$(".skill").fadeIn(500);
-		} else {
-			$(".skill[data-skill-type='"+checkedValue+"']").fadeIn(500);
-		}
-	});
-
-	var uiTag = `
-		<div class="work-tag work-tag--design">
-			<i class="bi bi-brush"></i>
-			<span>UI/UX Design</span>
-		</div>
-	`;
-
-	var figmaTag = `
-		<div class="work-tag work-tag--design">
-			<i class="fab fa-figma"></i>
-			<span>Figma</span>
-		</div>
-	`;
-
-	var htmlTag = `
-		<div class="work-tag work-tag--dev">
-			<i class="fab fa-html5"></i>
-			<span>HTML</span>
-		</div>
-	`;
-
-	var cssTag = `
-		<div class="work-tag work-tag--dev">
-			<i class="fab fa-css3"></i>
-			<span>CSS3</span>
-		</div>
-	`;
-
-	var jsTag = `
-		<div class="work-tag work-tag--dev">
-			<i class="bi bi-code-slash"></i>
-			<span>JavaScript</span>
-		</div>
-	`;
-
-	var desktopTag = `
-		<div class="work-tag work-tag--design">
-			<i class="bi bi-display"></i>
-			<span>Desktop Design</span>
-		</div>
-	`;
-
-	var mobileTag = `
-		<div class="work-tag work-tag--design">
-			<i class="bi bi-phone"></i>
-			<span>Mobile Design</span>
-		</div>
-	`;
-
-	var responsiveTag = `
-		<div class="work-tag work-tag--design">
-			<i class="bi bi-phone"></i>
-			<span>Responsive Design</span>
-		</div>
-	`;
-
-	var nocodeTag = `
-		<div class="work-tag work-tag--dev">
-			<i class="bi bi-hand-index"></i>
-			<span>No-Code Platform</span>
-		</div>
-	`;
-
-	var uxrTag = `
-		<div class="work-tag work-tag--design">
-			<i class="bi bi-people"></i>
-			<span>User Research</span>
-		</div>
-	`;
-
-	const tagList = [
-		{
-			uidesign: uiTag,
-			figma: figmaTag,
-			html: htmlTag,
-			css: cssTag,
-			javascript: jsTag,
-			desktop: desktopTag,
-			mobile: mobileTag,
-			responsive: responsiveTag,
-			nocode: nocodeTag,
-			uxr: uxrTag
-		}
-	];
-
-	// Add all tags
-	$(".work-tag-outer").each(function() {
-		var currentTagOuter = $(this);
-		var tagName = String(currentTagOuter.data("tag-name"));
-		// Find the relevant item in the tagList array and append it
-		currentTagOuter.append(tagList[0][tagName]);
-	});
+	// Set the value
+	$("#years-experience").text(yearsExperience);
 
 });
